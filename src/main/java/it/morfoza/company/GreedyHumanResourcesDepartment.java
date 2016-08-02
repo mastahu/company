270bc39c3e6295b507c3699932dbfdf4c1434416
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-08-02 16:55:23 +0200

    GreedyHumanResourcesDepartment

---
+++ src/main/java/it/morfoza/company/GreedyHumanResourcesDepartment.java
@@ -0,0 +1,11 @@
+package it.morfoza.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GreedyHumanResourcesDepartment implements HumanResourcesDepartment {
+    @Override
+    public List<Employee> getEmployeesForRise(List<Employee> employees) {
+        return new ArrayList<>();
+    }
+}
