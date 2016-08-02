3644baf62966539d5fa7201b9847c56bc5b19587
Anna Pazera <anna.pazera@tlen.pl>
2016-08-02 16:55:44 +0200

    dodana moja klasa AniaHRD

---
+++ src/main/java/it/morfoza/company/AniaHumanResourcesDepartment.java
@@ -0,0 +1,19 @@
+package it.morfoza.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Anna Kacprzak on 2016-08-02.
+ */
+public class AniaHumanResourcesDepartment implements HumanResourcesDepartment {
+
+//            AniaHumanResourcesDepartment givenRises= new AniaHumanResourcesDepartment();
+
+    @Override
+    public List<Employee> getEmployeesForRise(List<Employee> employees) {
+
+return new ArrayList<>();
+    }
+}
+
