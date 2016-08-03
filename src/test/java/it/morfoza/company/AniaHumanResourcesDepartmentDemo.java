d746b38091e78765d0bb46514e8188f69d6dd055
Anna Pazera <anna.pazera@tlen.pl>
2016-08-03 12:12:28 +0200

    second methind with contains added

--- src/test/java/it/morfoza/company/AniaHumanResourcesDepartmentDemo.java
+++ src/test/java/it/morfoza/company/AniaHumanResourcesDepartmentDemo.java
@@ -2,6 +2,7 @@ package it.morfoza.company;
 
 import java.util.List;
 
+import static it.morfoza.company.DemoUtil.check;
 import static java.util.Arrays.asList;
 
 /**
@@ -9,23 +10,50 @@ import static java.util.Arrays.asList;
  */
 public class AniaHumanResourcesDepartmentDemo {
     public static void main(String[] args) {
-        HumanResourcesDepartment hr = new AniaHumanResourcesDepartment();
-
-        List<Employee> employeesForRise;
-        employeesForRise  = hr.getEmployeesForRise(asList(lazyWoman()));
-        DemoUtil.check(employeesForRise.size() == 1);
-        employeesForRise  = hr.getEmployeesForRise(asList(notWoman()));
-        DemoUtil.check(employeesForRise.size() == 0);
-        employeesForRise  = hr.getEmployeesForRise(asList(hardWorkingWoman()));
-        DemoUtil.check(employeesForRise.size() == 0);
+//        test1();
+        test2();
     }
+
     private static Employee lazyWoman() {
         return new Employee("Anna", "Pazera", 5000, "JAVA programmer", false, Gender.FEMALE);
     }
-    private static Employee hardWorkingWoman(){
+
+    private static Employee hardWorkingWoman() {
         return new Employee("Kasia", "Sobczak", 1000, "cleaner", true, Gender.FEMALE);
     }
+
     private static Employee notWoman() {
-        return new Employee("Marek", "Szymczak", 2000,"Cofee maker", true, Gender.MALE);
+        return new Employee("Marek", "Szymczak", 2000, "Cofee maker", true, Gender.MALE);
+
+    }
+
+    private static void test1() {
+        HumanResourcesDepartment hr = new AniaHumanResourcesDepartment();
+
+        List<Employee> employeesForRise;
+        employeesForRise = hr.getEmployeesForRise(asList(lazyWoman()));
+        check(employeesForRise.size() == 1);
+
+        employeesForRise = hr.getEmployeesForRise(asList(notWoman()));
+        check(employeesForRise.size() == 0);
+
+        employeesForRise = hr.getEmployeesForRise(asList(hardWorkingWoman()));
+        check(employeesForRise.size() == 0);
+    }
+
+    private static void test2() {
+        HumanResourcesDepartment hr = new AniaHumanResourcesDepartment();
+//given
+        List<Employee> employeesForRise;
+
+        Employee lazyWoman = lazyWoman();
+        Employee notWoman = notWoman();
+//When
+        employeesForRise = hr.getEmployeesForRise(asList(lazyWoman, notWoman));
+// Then
+
+        check(employeesForRise.size() == 1);
+        check(employeesForRise.contains(lazyWoman));
+        check(!employeesForRise.contains(notWoman));
     }
 }
