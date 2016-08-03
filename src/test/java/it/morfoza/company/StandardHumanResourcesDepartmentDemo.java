01cff29eecc2cc1b3be40673dd9b8523ee0cd8d2
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-08-03 13:42:36 +0200

    Refactoring structure of StandardHumanResourcesDepartmentDemo

--- src/test/java/it/morfoza/company/StandardHumanResourcesDepartmentDemo.java
+++ src/test/java/it/morfoza/company/StandardHumanResourcesDepartmentDemo.java
@@ -7,26 +7,55 @@ import static java.util.Arrays.asList;
 
 public class StandardHumanResourcesDepartmentDemo {
     public static void main(String[] args) {
-        HumanResourcesDepartment hr = new StandardHumanResourcesDepartment();
 
-        List<Employee> employeesForRise;
-        employeesForRise = hr.getEmployeesForRise(asList(hardWorkingEmployee()));
-        check(employeesForRise.size() == 1);
+        HumanResourcesDepartment sut = new StandardHumanResourcesDepartment();
 
-        employeesForRise = hr.getEmployeesForRise(asList(lazyEmployee()));
-        check(employeesForRise.size() == 0);
+        should_give_rise_to_hard_working_employee(sut);
 
+        should_not_give_rise_to_lazy_employee(sut);
 
+        should_give_rise_to_hardworking_emoployees_only(sut);
 
-        Employee hardWorkingEmployee = hardWorkingEmployee();
-        Employee lazyEmployee = lazyEmployee();
-
-        employeesForRise = hr.getEmployeesForRise(asList(hardWorkingEmployee, lazyEmployee));
-        check(employeesForRise.size() == 1);
-        check(employeesForRise.contains(hardWorkingEmployee));
-        check(!employeesForRise.contains(lazyEmployee));
 
     }
+
+    private static void should_give_rise_to_hardworking_emoployees_only(HumanResourcesDepartment sut) {
+        // Given
+        Employee hardWorkingEmployee = hardWorkingEmployee();
+        Employee lazyEmployee = lazyEmployee();
+        List<Employee> candidatesForRise = asList(hardWorkingEmployee, lazyEmployee);
+
+        // When
+        List<Employee> approvedForRise = sut.getEmployeesForRise(candidatesForRise);
+
+        // Then
+        check(approvedForRise.size() == 1);
+        check(approvedForRise.contains(hardWorkingEmployee));
+        check(!approvedForRise.contains(lazyEmployee));
+    }
+
+    private static void should_not_give_rise_to_lazy_employee(HumanResourcesDepartment sut) {
+        // Given
+        List<Employee> candidatesForRise = asList(lazyEmployee());
+
+        // When
+        List<Employee> approvedForRise = sut.getEmployeesForRise(candidatesForRise);
+
+        // Then
+        check(approvedForRise.size() == 0);
+    }
+
+    private static void should_give_rise_to_hard_working_employee(HumanResourcesDepartment sut) {
+        // Given
+        List<Employee> candidatesForRise = asList(hardWorkingEmployee());
+
+        // When
+        List<Employee> approvedForRise = sut.getEmployeesForRise(candidatesForRise);
+
+        // Then
+        check(approvedForRise.size() == 1);
+    }
+
 
     private static Employee hardWorkingEmployee() {
         return createEmployeeWithHardWorkingFieldSetTo(true);
