88b03841bcd2c48c6e7e39ca1c130a3e73bb173f
Karola <yumi.chan@interia.pl>
2016-08-03 15:16:28 +0200

    TralalaHumanResourcesDepartmentDemo

---
+++ src/test/java/it/morfoza/company/TralalaHumanResourcesDepartmentDemo.java
@@ -0,0 +1,49 @@
+package it.morfoza.company;
+
+import it.morfoza.company.Employee;
+import it.morfoza.company.HumanResourcesDepartment;
+
+import java.util.List;
+
+import static it.morfoza.company.DemoUtil.check;
+import static it.morfoza.company.Gender.FEMALE;
+import static it.morfoza.company.Gender.MALE;
+import static java.util.Arrays.asList;
+
+public class TralalaHumanResourcesDepartmentDemo {
+
+
+    public static void main(String[] args) {
+
+        HumanResourcesDepartment humanResourcesDeprartment = null;
+        //do zmiennej o typie HumanResourcesDepartment i
+        // nazwie humanResourcesDeprartment przypisujemy nowo utworzony obiekt typu new NewHumanResourcesDepartment
+
+
+        // given
+        Employee woman = employeeWithGender(FEMALE);
+        Employee man = employeeWithGender(MALE);
+        List<Employee> candidatesForRise = asList(woman, man);
+
+
+        // when
+        List<Employee>  approvedForRise; //deklaruje zmienna o typie List<Employee> o nazwie approvedForRise
+        approvedForRise = humanResourcesDeprartment.getEmployeesForRise(candidatesForRise);
+
+
+        // then
+        check(approvedForRise.size() == 1 ); //zwraca rozmiar listy
+        check(approvedForRise.contains(woman)); //sprawdza czy zawiera podany obiekt
+        check(!approvedForRise.contains(man));//then
+
+
+    }
+
+
+    private static Employee employeeWithGender (Gender gender) {
+        return new Employee ("Kasia", "Kowalska", 2000.0, "", false, gender);
+
+    }
+
+
+}
