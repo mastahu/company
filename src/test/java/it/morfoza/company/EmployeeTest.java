ab01956e59665389b0e94c0192e7a9a9f075b4fd
widzew <klop99@wp.pl>
2016-08-12 15:47:23 +0200

    EmployeeTestJs

--- src/test/java/it/morfoza/company/EmployeeTest.java
+++ src/test/java/it/morfoza/company/EmployeeTest.java
@@ -10,16 +10,16 @@ public class EmployeeTest {
     public void testEquals() {
 
         String anna = "";
-        if (true) {
+        if(true) {
             anna += "Anna";
         }
 
         assertEquals(anna, "Anna");
 
-        Employee employee1 = new Employee(anna, "Pazera", 5000, "junior", true, Gender.FEMALE);
-        Employee employee2 = new Employee("Anna", "Pazera", 5000, "junior", true, Gender.FEMALE);
-        Employee employee3 = new Employee("Piotr", "Przybylak", 15000, "senior", false, Gender.MALE);
-        Employee employee4 = new Employee("Piotr", "Kowalski", 15000, "senior", false, Gender.MALE);
+        Employee employee1=new Employee(anna, "Pazera", 5000, "junior", true, Gender.FEMALE);
+        Employee employee2=new Employee("Anna", "Pazera", 5000, "junior", true, Gender.FEMALE);
+        Employee employee3=new Employee("Piotr", "Przybylak", 15000, "senior", false, Gender.MALE);
+        Employee employee4=new Employee("Piotr", "Kowalski", 15000, "senior", false, Gender.MALE);
         assertEquals(employee1, employee2);
         assertTrue(employee1.equals(employee2));
         assertNotEquals(employee1, employee3);
@@ -46,5 +46,7 @@ public class EmployeeTest {
     public void testEmployeePP() {
 
     }
+    @Test
+    public void testEmployeeJS(){}
 
 }
